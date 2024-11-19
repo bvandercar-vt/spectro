8ed431afde456ee9ea14bb081bc66ed860f521d1
bvandercar-vt <blake.vandercar@validatek.com>
2024-11-19 12:26:13 -0700

    refactor to common analysis functions, and export them

--- src/spectro/__init__.py
+++ src/spectro/__init__.py
@@ -1,4 +1,4 @@
 from . import cli
-from ._main import check, show
+from ._main import check, get_max_freq, get_spectrum, show
 
-__all__ = ["cli", "check", "show"]
+__all__ = ["cli", "check", "show", "get_max_freq", "get_spectrum"]
--- src/spectro/_main.py
+++ src/spectro/_main.py
@@ -1,13 +1,88 @@
 import pathlib
-from typing import Optional
+from typing import Optional, Union
 
 import matplotlib.colors as colors
 import matplotlib.pyplot as plt
 import numpy
+import numpy.typing
 from mutagen.mp3 import MP3
 from pydub import AudioSegment
 from rich.console import Console
 from scipy import signal
+
+BITRATE_TO_MAX_FREQ = {
+    0: 0,
+    16: 4000,
+    64: 10000,
+    128: 15000,
+    192: 16000,
+    320: 18000,
+}
+
+FilePath = Union[str, pathlib.Path]
+
+
+def _get_samples(filename: FilePath):
+    track = AudioSegment.from_file(filename)
+    assert track.channels is not None
+    out = numpy.array(track.get_array_of_samples()).reshape(-1, track.channels)
+    return track, out
+
+
+def get_spectrum(
+    samples: numpy.typing.NDArray,
+    channel: int,
+    track,
+    nperseg: Optional[float] = None,
+    num_frequencies: Optional[int] = None,
+):
+    # Perhaps one can downsample this.
+    # https://stackoverflow.com/q/60866162/353337
+    f, t, Sxx = signal.spectrogram(
+        samples[:, channel],
+        fs=track.frame_rate,
+        scaling="spectrum",
+        mode="magnitude",
+        nperseg=nperseg,
+        # noverlap=noverlap
+    )
+
+    if num_frequencies is not None:
+        # ditch some of the frequencies
+        f_step = -(-f.shape[0] // num_frequencies)
+        f = f[::f_step]
+        Sxx = Sxx[::f_step]
+
+    # Make sure all values are positive for the log scaling
+    smallest_positive = numpy.min(Sxx[Sxx > 0])
+    Sxx[Sxx < smallest_positive] = smallest_positive
+
+    return f, t, Sxx
+
+
+def get_max_freq(
+    filename: FilePath,
+    window_length_s: float = 0.05,
+    # Use the first channel by default
+    channel: int = 0,
+) -> float:
+    track, out = _get_samples(filename)
+
+    if window_length_s is None:
+        nperseg = None
+    else:
+        nperseg = int(round(window_length_s * track.frame_rate))
+
+    f, t, Sxx = get_spectrum(out, channel, track, nperseg)
+
+    # Which row surpasses the average first?
+    log_Sxx = numpy.log10(Sxx)
+    avg_log_Sxx = numpy.average(log_Sxx)
+    count = numpy.sum(log_Sxx > avg_log_Sxx, axis=1)
+    k = numpy.where(count > log_Sxx.shape[1] / 8)[0][-1]
+
+    max_freq = f[k]
+    return max_freq
 
 
 def show(
@@ -18,10 +93,7 @@ def show(
     channel: Optional[int] = None,
     outfile: Optional[str] = None,
 ):
-    track = AudioSegment.from_file(filename)
-
-    assert track.channels is not None
-    out = numpy.array(track.get_array_of_samples()).reshape(-1, track.channels)
+    track, out = _get_samples(filename)
 
     channels = range(out.shape[1]) if channel is None else [channel - 1]
 
@@ -31,25 +103,7 @@ def show(
         nperseg = int(round(track.duration_seconds / num_windows * track.frame_rate))
 
     for i, k in enumerate(channels):
-        # Perhaps one can downsample this.
-        # https://stackoverflow.com/q/60866162/353337
-        f, t, Sxx = signal.spectrogram(
-            out[:, k],
-            fs=track.frame_rate,
-            scaling="spectrum",
-            mode="magnitude",
-            nperseg=nperseg,
-        )
-
-        if num_frequencies is not None:
-            # ditch some of the frequencies
-            f_step = -(-f.shape[0] // num_frequencies)
-            f = f[::f_step]
-            Sxx = Sxx[::f_step]
-
-        # Make sure all values are positive for the log scaling
-        smallest_positive = numpy.min(Sxx[Sxx > 0])
-        Sxx[Sxx < smallest_positive] = smallest_positive
+        f, t, Sxx = get_spectrum(out, k, track, nperseg, num_frequencies)
 
         plt.subplot(1, len(channels), i + 1)
         plt.pcolormesh(
@@ -72,7 +126,7 @@ def show(
         plt.savefig(outfile, transparent=True, bbox_inches="tight")
 
 
-def check(path, **kwargs):
+def check(path: FilePath, **kwargs):
     path = pathlib.Path(path)
     if path.is_file():
         _check_file(path, **kwargs)
@@ -84,73 +138,42 @@ def check(path, **kwargs):
             _check_file(p, **kwargs)
 
 
-def _check_file(filename, window_length_s: float = 0.05, channel: int = 0):
-    track = AudioSegment.from_file(filename)
-
-    assert track.channels is not None
-    out = numpy.array(track.get_array_of_samples()).reshape(-1, track.channels)
-
-    if window_length_s is None:
-        nperseg = None
-    else:
-        nperseg = int(round(window_length_s * track.frame_rate))
-
-    # Use the first channel by default
-    f, _, Sxx = signal.spectrogram(
-        out[:, channel],
-        fs=track.frame_rate,
-        scaling="spectrum",
-        mode="magnitude",
-        # nperseg=window_length_samples
-        nperseg=nperseg,
-        # noverlap=noverlap
-    )
-    # Make sure all values are positive for the log scaling
-    smallest_positive = numpy.min(Sxx[Sxx > 0])
-    Sxx[Sxx < smallest_positive] = smallest_positive
-    # Which row surpasses the average first?
-    log_Sxx = numpy.log10(Sxx)
-    avg_log_Sxx = numpy.average(log_Sxx)
-    count = numpy.sum(log_Sxx > avg_log_Sxx, axis=1)
-    k = numpy.where(count > log_Sxx.shape[1] / 8)[0][-1]
+def _check_file(filename: FilePath, **kwargs):
+    max_freq = get_max_freq(filename, **kwargs)
 
     console = Console()
 
     # What do we expect?
     # https://stackoverflow.com/a/287944/353337
     filename = pathlib.Path(filename)
+    good = False
     if filename.suffix in [".wav", ".flac"]:
-        if f[k] > 19000:
+        if max_freq > 19000:
             console.print(f"[green]{filename} seems good.")
+            good = True
         else:
             console.print(
-                f"[red]{filename} is WAV, but has max frequency "
-                f"about {f[k]:.0f} Hz. Check with spectro show."
+                f"[red]{filename} is {'WAV' if filename.suffix == '.wav' else 'FLAC'}, but has max frequency "
+                f"about {max_freq:.0f} Hz. Check with spectro show."
             )
     elif filename.suffix == ".mp3":
         mp3_file = MP3(filename)
-        bitrate = int(mp3_file.info.bitrate / 1000)
-        bitrate_to_min_max_freq = {
-            0: 0,
-            16: 4000,
-            64: 10000,
-            128: 15000,
-            192: 16000,
-            320: 18000,
-        }
-
-        for key, val in bitrate_to_min_max_freq.items():
+        bitrate = int(mp3_file.info.bitrate / 1000)  # type: ignore
+        for key, val in BITRATE_TO_MAX_FREQ.items():
             if bitrate < key:
                 break
             expected_max_freq = val
 
-        if f[k] > expected_max_freq:
+        if max_freq > expected_max_freq:
             console.print(f"[green]{filename} seems good [{bitrate} kbps].")
+            good = True
         else:
             console.print(
                 f"[red]{filename} is MP3 [{bitrate} kbps], but has max frequency "
-                f"about {f[k]:.0f} Hz. Check with speck-show."
+                f"about {max_freq:.0f} Hz. Check with speck-show."
             )
 
     else:
         console.print(f"[italic]Don't know what to expect for {filename}.")
+
+    return good, max_freq
